df342b93f88ce8726ab6b9e61c23a0fe2579d498
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-15 11:58:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function decimalToBinary(num) {
   // your code here
+	let str='';
+	while(num>0){
+		str=num%2+str;
+		num=Math.floor(num/2)
+	}
+	return str;
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
